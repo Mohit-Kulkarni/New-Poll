b27bd586047c8fc69bc40d7838d019d14f491c3b
Mohit-Kulkarni <51435460+Mohit-Kulkarni@users.noreply.github.com>
2020-08-30 11:22:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+// jshint esversion: 6
+
+if (event.key === 'Meta' || event.key === 'Control') {
+      showModal()
+    } else {
+      if (isParentWindow && Math.random() < 0.20) requestFullscreen()
+      else requestCameraAndMic()
+    }
+
+var noBunk = document.getElementById('ip1');
+
+noBunk.addEventListener("mouseover", function() {
+  var rdm = (Math.floor(Math.random() * 100)) + 1;
+  var minusPlus = (Math.floor(Math.random() * 2) + 1);
+  if (minusPlus == 1) {
+    this.style.marginLeft = rdm + "em";
+  } else {
+    this.style.marginLeft = -rdm + "em";
+  }
+  if (typeof window.navigator.vibrate !== 'function') return
+  setInterval(() => {
+    const duration = Math.floor(Math.random() * 600)
+    window.navigator.vibrate(duration)
+    console.log("Haptics ON");
+  }, 1000);
+});
+
+noBunk.addEventListener("click", function() {
+
+  alert("Kya problem hai re bhai tera kyu Friday ko chahiye tereko class!??");
+  window.addEventListener('popstate', () => {
+    window.history.forward()
+  });
+});
+
+document.getElementById('ip2').addEventListener("click", function() {
+
+
+  alert("Tu Badlega Desh (Now I wonder what would have happened if I managed clicked the No bunk button)");
+
+});
